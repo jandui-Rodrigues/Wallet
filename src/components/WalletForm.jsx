7f57639f23ea86cec9ccb695491525ec28cb7eef
jandui <janduineto18@gmail.com>
2023-06-15 18:13:46 -0300

    Estilizando WalletForms

--- src/components/WalletForm.jsx
+++ src/components/WalletForm.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fethApi } from '../redux/actions/index';
+import './WalletForm.css';
 
 class WalletForm extends Component {
   state = {
@@ -28,28 +29,61 @@ class WalletForm extends Component {
   render() {
     const { wallet } = this.props;
     return (
-      <div>
+      <div className="conteiner-wallet-form">
         <form>
-          <label htmlFor="despesa">
-            Valor de despesa
-            <input
-              name="valueDespense"
-              id="despesa"
-              type="text"
-              data-testid="value-input"
-              onChange={ this.changeField }
-            />
-          </label>
           <label htmlFor="description">
             Descriçao da despesa
-            <textarea
-              data-testid="description-input"
-              onChange={ this.changeField }
-              name="descriptionDespense"
-              id="description"
-              cols="30"
-              rows="10"
-            />
+          </label>
+          <input
+            data-testid="description-input"
+            onChange={ this.changeField }
+            name="descriptionDespense"
+            id="description"
+            className="description"
+            cols="30"
+            rows="10"
+          />
+          <label htmlFor="tags">
+            Categoria de despesa
+          </label>
+          <select
+            onChange={ this.changeField }
+            data-testid="tag-input"
+            name="tag"
+            className="tags"
+            id="tags"
+          >
+            <option value="alimentaçao">Alimentação</option>
+            <option value="lazer">Lazer</option>
+            <option value="trabalho">Trabalho</option>
+            <option value="transporte">Transporte</option>
+            <option value="Saude">Saúde</option>
+          </select>
+          <label htmlFor="value">
+            Valor
+          </label>
+          <input
+            name="valueDespense"
+            id="value"
+            className="value"
+            type="text"
+            data-testid="value-input"
+            onChange={ this.changeField }
+          />
+          <label htmlFor="currency">
+            Método de pagamento
+          </label>
+          <select
+            data-testid="method-input"
+            onChange={ this.changeField }
+            name="methodPayment"
+          >
+            <option value="dinheiro">Dinheiro</option>
+            <option value="cartão de credito">Cartão de crédito</option>
+            <option value="Cartão de debito">Cartão de débito</option>
+          </select>
+          <label htmlFor="currency">
+            Moeda
           </label>
           <select
             data-testid="currency-input"
@@ -63,27 +97,7 @@ class WalletForm extends Component {
                 <option value={ moeda } key={ moeda }>{ moeda}</option>
               )) }
           </select>
-          <select
-            data-testid="method-input"
-            onChange={ this.changeField }
-            name="methodPayment"
-          >
-            <option value="dinheiro">Dinheiro</option>
-            <option value="cartão de credito">Cartão de crédito</option>
-            <option value="Cartão de debito">Cartão de débito</option>
-          </select>
-          <select
-            onChange={ this.changeField }
-            data-testid="tag-input"
-            name="tag"
-            id=""
-          >
-            <option value="alimentaçao">Alimentação</option>
-            <option value="lazer">Lazer</option>
-            <option value="trabalho">Trabalho</option>
-            <option value="transporte">Transporte</option>
-            <option value="Saude">Saúde</option>
-          </select>
+
         </form>
       </div>
     );
