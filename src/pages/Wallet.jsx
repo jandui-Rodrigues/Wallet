2f1ed4467741ba44cdcd5f3180313746a7694bc9
jandui <janduineto18@gmail.com>
2023-06-16 17:24:48 -0300

    Requisito 6

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
 import './Wallet.css';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
     return (
       <div className="conteiner-wallet">
-
         <Header />
         <WalletForm />
+        <Table />
       </div>
     );
   }
