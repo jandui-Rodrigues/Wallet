a3c8f89fa139bdc93247c4e9a9c66d339802920e
jandui <janduineto18@gmail.com>
2023-11-14 14:59:13 -0300

    style: chage tables

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -12,54 +12,56 @@ class Table extends Component {
     const { dispatch } = this.props;
     console.log(expenses);
     return (
-      <table className="table-despense">
-        <thead className="table-head">
+      <div className="conteiner-table">
+        <table className="table-despense">
+          <thead className="table-head">
 
-          <tr>
+            <tr>
 
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
-        </thead>
-        <tbody className="table-body">
-          { expenses.map(({
-            id, description, tag, method, value, currency, exchangeRates,
-          }) => (
-            <tr key={ id }>
-              <td>{description}</td>
-              <td>{tag}</td>
-              <td>{method}</td>
-              <td>{Number(value).toFixed(2)}</td>
-              <td>{exchangeRates[currency].name}</td>
-              <td>{ Number(exchangeRates[currency].ask).toFixed(2)}</td>
-              <td>{ (exchangeRates[currency].ask * value).toFixed(2) }</td>
-              <td>Real</td>
-              <td>
-                <button
-                  data-testid="edit-btn"
-                  className="button-edit"
-                  onClick={ () => dispatch(editExpenseTrue(id)) }
-                >
-                  <img src={ edit } alt="" />
-                </button>
-                <button
-                  data-testid="delete-btn"
-                  onClick={ () => dispatch(deleteExpense(id)) }
-                >
-                  <img src={ deleteIcon } alt="" />
-                </button>
-              </td>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="table-body">
+            { expenses.map(({
+              id, description, tag, method, value, currency, exchangeRates,
+            }) => (
+              <tr key={ id }>
+                <td>{description}</td>
+                <td>{tag}</td>
+                <td>{method}</td>
+                <td>{Number(value).toFixed(2)}</td>
+                <td>{exchangeRates[currency].name}</td>
+                <td>{ Number(exchangeRates[currency].ask).toFixed(2)}</td>
+                <td>{ (exchangeRates[currency].ask * value).toFixed(2) }</td>
+                <td>Real</td>
+                <td className="buttons">
+                  <button
+                    data-testid="edit-btn"
+                    className="button-edit"
+                    onClick={ () => dispatch(editExpenseTrue(id)) }
+                  >
+                    <img src={ edit } alt="" />
+                  </button>
+                  <button
+                    data-testid="delete-btn"
+                    onClick={ () => dispatch(deleteExpense(id)) }
+                  >
+                    <img src={ deleteIcon } alt="" />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
